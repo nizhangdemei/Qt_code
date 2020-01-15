44035941454be13cd03e1cfb84aed0f6551125b7
nizhangdemei <2940983806@qq.com>
2020-01-15 10:36:54 +0800

    2020/01/15

--- src/Qt-quick/myMdi/mainwindow.cpp
+++ src/Qt-quick/myMdi/mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QMdiSubWindow>
+#include "mdichild.h"
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    actionSeparator = new QAction(this);
+    actionSeparator->setSeparator(true);
+    updateMenus();
+
+    connect(ui->mdiArea,&QMdiArea::subWindowActivated,this,&MainWindow::updateMenus);\
 
 
 }
@@ -13,4 +19,49 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::updateMenus()
+{
+    //根据是否有活动窗口来设置各动作是否可用
+    bool hasMdiChild = (this->activeMdiChild()!= nullptr);
+    ui->actionSave->setEnabled(hasMdiChild);
+    ui->actionSaveAS->setEnabled(hasMdiChild);
+    ui->actionPaste->setEnabled(hasMdiChild);
+    ui->actionClose->setEnabled(hasMdiChild);
+    ui->actionCloseAll->setEnabled(hasMdiChild);
+    ui->actionTile->setEnabled(hasMdiChild);
+    ui->actionCascade->setEnabled(hasMdiChild);
+    ui->actionNext->setEnabled(hasMdiChild);
+    ui->actionPreview->setEnabled(hasMdiChild);
+    //设置间隔期是否显示
+    actionSeparator->setVisible(hasMdiChild);
+    //有活动窗口并且有被选择的文本，剪切复制才可用
+    bool hasSelection = (activeMdiChild() && activeMdiChild()->textCursor().hasSelection());
+    ui->actionCut->setEnabled(hasSelection);
+    ui->actionCopy->setEnabled(hasSelection);
+    //有活动窗口并且文档有撤销操作撤销动作可用
+    ui->actionUndo->setEnabled(activeMdiChild() && activeMdiChild()->document()->isUndoAvailable());
+    //有活动窗口并且文档有恢复操作时恢复动作可用
+    ui->actionRedo->setEnabled(activeMdiChild() && activeMdiChild()->document()->isRedoAvailable());
+
+}
+
+void MainWindow::on_actionNew_triggered()
+{
+    MdiChild *child = new MdiChild();
+    ui->mdiArea->addSubWindow(child);
+    //新建文件
+    child->newFile();
+    child->show();
+}
+
+MdiChild *MainWindow::activeMdiChild()
+{
+    if(QMdiSubWindow *activeSubWindos = ui->mdiArea->activeSubWindow())
+    {
+        return qobject_cast<MdiChild *>(activeSubWindos->widget());
+    }
+    return nullptr;
+
 }
--- src/Qt-quick/myMdi/mainwindow.h
+++ src/Qt-quick/myMdi/mainwindow.h
@@ -1,8 +1,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <QAction>
 #include <QMainWindow>
-
+class MdiChild;
 namespace Ui {
 class MainWindow;
 }
@@ -14,9 +14,16 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void updateMenus(); //更新菜单
+
+    private slots:
+        void on_actionNew_triggered();
 
 private:
     Ui::MainWindow *ui;
+    QAction *actionSeparator;
+    MdiChild * activeMdiChild();//活动窗口
+
 };
 
 #endif // MAINWINDOW_H
